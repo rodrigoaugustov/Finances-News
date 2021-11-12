0e80f6cf488731325b037b4482710e7f7e942ffb
f8711697 <rodrigoaugustov@bb.com.br>
2021-11-12 19:13:38 -0300

    Create BR Investing Spyder

--- finances/settings.py
+++ finances/settings.py
@@ -12,6 +12,7 @@ BOT_NAME = 'finances'
 SPIDER_MODULES = ['finances.spiders']
 NEWSPIDER_MODULE = 'finances.spiders'
 
+FEED_EXPORT_ENCODING = 'utf-8'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'finances (+http://www.yourdomain.com)'
---
+++ finances/spiders/brinvesting.py
@@ -0,0 +1,38 @@
+import scrapy
+import re
+from datetime import datetime
+
+
+class InvestingSpider(scrapy.Spider):
+    name = 'brinvesting'
+    base_url = 'https://br.investing.com'
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+
+    def start_requests(self):
+        urls = [
+            'https://br.investing.com/news/stock-market-news',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, headers=self.headers, callback=self.parse)
+
+    def parse(self, response):
+        urls = response.xpath('//a[contains(@class, "title")]/@href').getall()
+        for link in urls:
+            if 'https' not in link:
+                yield response.follow(url=f'{self.base_url}{link}', headers=self.headers, callback=self.parse_article)
+
+    def parse_article(self, response):
+        title = response.xpath('//title/text()').get()
+        title = re.sub(r'Por.*', '', title).strip()
+        divs = response.css(".WYSIWYG.articlePage")
+        content = " ".join([p.get() for p in divs.xpath('.//*/text()')])
+        time = response.css('.contentSectionDetails span::text').get()
+        if "(" in time:
+            time = time.split('(')[1].split(')')[0]
+        time = datetime.strptime(time, '%d.%m.%Y %H:%M')
+
+        yield {
+            "title": title,
+            "time": time,
+            "content": content
+        }
